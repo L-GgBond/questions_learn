7d494c456d708265c8b9d6a223ec225db0a764d5
404 <948472619@qq.com>
2026-02-26 11:23:36 +0800

    配置单元测试

--- src/test/Cases/ExampleTest.php
+++ src/test/Cases/ExampleTest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace HyperfTest\Cases;
 
 use Hyperf\Testing\TestCase;
+use function Hyperf\Coroutine\run;
 
 /**
  * @internal
@@ -22,6 +23,22 @@ class ExampleTest extends TestCase
 {
     public function testExample()
     {
-        $this->get('/')->assertOk()->assertSee('Hyperf');
+        \Hyperf\Coroutine\run(function () {
+            // 1. 获取响应对象
+            $response = $this->get('/');
+
+            // 2. 将响应对象转换为数组（Hyperf 的 TestResponse 提供了便捷方法）
+            $res = $response->json();
+
+            // 3. 执行断言
+            $this->assertIsArray($res);
+            $this->assertSame('Hello Hyperf.', $res['message'] ?? '');
+            $this->assertSame('GET', $res['method'] ?? '');
+        });
+    }
+
+    public function testTrue()
+    {
+        $this->assertTrue(true);
     }
 }
